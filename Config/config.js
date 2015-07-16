88b7d064a77aee60747c1ffa98b88ed8b3f93036
Cncndog <jeffreyshencc@gmail.com>
2015-07-15 20:12:40 -0700

    Config

---
+++ Config/config.js
@@ -0,0 +1,21 @@
+function saveOptions() {
+    var name = document.getElementById("name");
+ 
+    var options = {
+      "name": name.options[name.selectedIndex].value
+    }
+     
+    return options;
+};
+ 
+var submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", 
+    function() {
+      console.log("Submit");
+ 
+      var options = saveOptions();
+      var location = "pebblejs://close#" + encodeURIComponent(JSON.stringify(options));
+       
+      document.location = location;
+    }, 
+false);
